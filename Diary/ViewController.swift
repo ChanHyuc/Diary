91de26e01a5c513d8b1533016c9df5f03fc0d6c8
ChanHyuc <whk1140@gmail.com>
2024-10-06 14:42:35 +0900

    Init: Diary 프로젝트 생성

---
+++ Diary/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by ChanHyuc on 10/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+    }
+}
+
